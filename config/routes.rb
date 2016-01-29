5f56c99cfb5e245de9535dc8878b9adc56cea873
Rails Udemy Course <railsudemycourse@gmail.com>
2016-01-29 00:25:51 -0500

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root 'pages#home'
-    #get 'pages/home', to: 'pages#home'
     get 'about', to: 'pages#about'
+    
+    resources :articles
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
